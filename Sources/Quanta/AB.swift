e254685817be8e1a94168b759ffbe7102e3a9259
Yannick <yannick.spreen@gmail.com>
2025-01-06 13:34:52 -0500

    Add abtest getter methods.

--- Sources/Quanta/AB.swift
+++ Sources/Quanta/AB.swift
@@ -9,6 +9,10 @@
 import Foundation
 
 extension Quanta {
+	public static func abTest(for experimentName: String) -> String {
+		abDict[experimentName.lowercased()] ?? "A"
+	}
+
 	nonisolated(unsafe) static var abLetters_: String = ""
 
 	static var abLetters: String {
@@ -20,20 +24,20 @@ extension Quanta {
 		}
 	}
 
-	nonisolated(unsafe) static var abNames_: [[String]] = []
+	nonisolated(unsafe) static var abDict_: [String: String] = [:]
 
-	static var abNames: [[String]] {
+	static var abDict: [String: String] {
 		get {
-			queue.sync { abNames_ }
+			queue.sync { abDict_ }
 		}
 		set {
-			queue.sync { abNames_ = newValue }
+			queue.sync { abDict_ = newValue }
 		}
 	}
 
 	static func set(abJson: String) {
 		abLetters = getAbLetters(for: abJson)
-		abNames = getAbNames(for: abJson)
+		abDict = getAbDict(for: abJson)
 	}
 }
 
@@ -64,9 +68,17 @@ func getAbLetters(for abJson: String) -> String {
 	return abLetters
 }
 
-func getAbNames(for abJson: String) -> [[String]] {
+func getAbDict(for abJson: String) -> [String: String] {
+	var dict = [String: String]()
 	guard let experiments = try? JSONDecoder().decode([ABExperiment].self, from: abJson.data(using: .utf8) ?? .init()) else {
-		return []
+		return dict
 	}
-	return experiments.map { $0.name.map { $0.lowercased() } }
+	let letters = Quanta.abLetters
+	for (idx, experiment) in experiments.enumerated() {
+		guard letters.count > idx else { break }
+		for name in experiment.name {
+			dict[name.lowercased()] = "\(letters.prefix(idx + 1).suffix(1))"
+		}
+	}
+	return dict
 }
